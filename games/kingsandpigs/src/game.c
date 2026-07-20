dac66fa9c4b8cdb4659e947947c65092619a6668
austinio7116 <mark.r.austin@gmail.com>
2026-07-20 17:11:12 +0100

    Kings and Pigs: v1.1.0 gallery update (louder floors, aggressive boss + cannon guard)

--- games/kingsandpigs/src/game.c
+++ games/kingsandpigs/src/game.c
@@ -1922,4 +1922,4 @@ static const MoteGameVtbl k_vtbl = {
 static const MoteGameVtbl *mote_game_vtbl(void) { return &k_vtbl; }
 
 MOTE_GAME_META("Kings and Pigs", "austinio7116");
-MOTE_GAME_VERSION("1.0.0");
+MOTE_GAME_VERSION("1.1.0");
